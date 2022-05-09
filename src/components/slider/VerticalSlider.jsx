739c087c22b94cd5508207f0584c616bee5eae63
Ojotule <benjamin.ojotule.ajodo@gmail.com>
2022-05-09 23:02:48 +0100

    added a slider component

---
+++ src/components/slider/VerticalSlider.jsx
@@ -0,0 +1,46 @@
+
+import React, { useState } from 'react'
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import './verticalSlider.css';
+
+
+export default function VerticalSlider({ onDataSet }) {
+  function preventHorizontalKeyboardNavigation(event) {
+    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+      event.preventDefault();
+    }
+  }
+  const [data, setData] = useState(5);
+  const handleChange = (event) => {
+    setData(event.target.value);
+    onDataSet(data)
+    console.log(onDataSet)   
+
+  }; 
+
+  return (
+    <Box sx={{ height: 210 }}>
+      <Slider
+        sx={{
+          '& input[type="range"]': {
+            WebkitAppearance: 'slider-vertical',
+          },
+        }}
+        onChange = {handleChange}
+        orientation="vertical"
+        //defaultValue={30}
+        aria-label="Temperature"
+        onKeyDown={preventHorizontalKeyboardNavigation}
+      />
+    </Box>
+  );
+}
+
+
+
+
+
+
+
+  
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,46 @@
 import './home.css'
-import React from 'react'
+import React, { useState } from 'react'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import Chart from '../../components/chart/Chart'
 import {userData} from "../../dummyData"
 import WidgetLg from '../../components/widgetLg/WidgetLg'
 import WidgetSm from '../../components/widgetSm/WidgetSm'
+import VerticalSlider from '../../components/slider/VerticalSlider'
+import '../../components/slider/verticalSlider.css';
+import ProgressBar from '../../components/progressBar/ProgressBar'
+
+
+
 
 const Home = () => {
+  const [sliderValue, setSliderValue] =useState(1);
+  function getSliderData(value){
+    setSliderValue(value);
+  }
+
+  //transforming the data
+  let newMonthValue = {}
+
+  const userDataTransform = userData.map(newDataFunc)
+  function newDataFunc(item){
+    const newActiveUserValue = sliderValue * item["Active User"];
+    newMonthValue = { ...item, "Active User":newActiveUserValue };
+    return newMonthValue
+  };
+  
+
   return (
     <div className='home'>
         <FeaturedInfo/>
-        <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+        <div className='chartContainer'>
+          <div className="SliderBar">
+            <VerticalSlider onDataSet={getSliderData}/>
+          </div>
+          <Chart data={userDataTransform} 
+          title="User Analytics" 
+          grid dataKey="Active User"/>
+          <ProgressBar/>
+        </div>
         <div className='homeWidgets'>
           <WidgetSm/>
           <WidgetLg/>
